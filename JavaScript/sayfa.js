dc6cbdbfb53e745734d5b1e292f433a81b16d196
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2024-03-28 13:17:32 +0300

    Çekmece kapanmaya ve duyarlılaşmaya başladı.

--- JavaScript/sayfa.js
+++ JavaScript/sayfa.js
@@ -2,14 +2,23 @@ let kullanıcı_çekmecesi_açık = false;
 
 function KullanıcıÇekmecesi()
 {
-    if(kullanıcı_çekmecesi_açık === true)
+    if(kullanıcı_çekmecesi_açık === true) // kapat
     {
         document.getElementById("kullanıcı-çekmecesi").style.width = "0";
+        document.getElementById("kapatıcı").style.display = "none"
         kullanıcı_çekmecesi_açık = false;
     }
-    else
+    else // aç
     {
-        document.getElementById("kullanıcı-çekmecesi").style.width = "25vw";
+        if (visualViewport.height <= visualViewport.width)
+        {
+            document.getElementById("kullanıcı-çekmecesi").style.width = "25vw";
+        }
+        else
+        {
+            document.getElementById("kullanıcı-çekmecesi").style.width = "100vw";
+        }
+        document.getElementById("kapatıcı").style.display = "block";
         kullanıcı_çekmecesi_açık = true;
     }
 }
