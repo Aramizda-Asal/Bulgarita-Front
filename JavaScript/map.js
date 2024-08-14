f65fe6b62725b9856c2e627052c44e59e6da6beb
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2024-08-14 16:17:27 +0300

    map.js'te sekme boyutunu 2'den 4'e çıkardım.

--- JavaScript/map.js
+++ JavaScript/map.js
@@ -16,40 +16,40 @@ function onEachFeature(feature, layer) {
 
 var BulgaristanŞehirNoktaGeo = 
 {
-  "type": "FeatureCollection",
-  "features":
-  [
-    {
-      "type": "Feature",
-      "properties": 
-      {
-        "label": "Pazarcık"
-      },
-      "geometry": 
-      {
-        "type": "Point",
-        "coordinates": [24.334235,42.192271]
-      }
-    },
-
-    {
-        "type": "Feature",
-        "properties": 
+    "type": "FeatureCollection",
+    "features":
+    [
         {
-          "label": "Filibe"
+            "type": "Feature",
+            "properties": 
+            {
+            "label": "Pazarcık"
+            },
+            "geometry": 
+            {
+            "type": "Point",
+            "coordinates": [24.334235,42.192271]
+            }
         },
-        "geometry": 
+
         {
-          "type": "Point",
-          "coordinates": [24.74551641878407,42.13585393690252]
+            "type": "Feature",
+            "properties": 
+            {
+                "label": "Filibe"
+            },
+            "geometry": 
+            {
+                "type": "Point",
+                "coordinates": [24.74551641878407,42.13585393690252]
+            }
         }
-      }
-  ]
+    ]
 };
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 var BulgaristanŞehirNokta = L.geoJSON(BulgaristanŞehirNoktaGeo, {onEachFeature: onEachFeature})
