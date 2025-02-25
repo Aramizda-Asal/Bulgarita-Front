d0f9a2e303fbfb6341592378e7ec8c4752e92e1e
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2025-02-25 16:22:27 +0300

    Rol gereken sayfa düğmeleri role göre oluşuyor. Sayfalar da gelecekte role göre oluşturulacak.

--- JavaScript/kişiselleştirici.js
+++ JavaScript/kişiselleştirici.js
@@ -75,6 +75,8 @@ function AnonimdenKişisele()
         }
     }
 
+    RoleGöreÖgeleriYarat();
+
     KÇAnaSayfa();
 }
 
@@ -84,6 +86,8 @@ function AnonimdenKişisele()
  */
 function KişiseldenAnonime()
 {
+    RoleGöreÖgeleriYokEt();
+
     let anonim_bölgeler = document.getElementsByClassName("anonim-bölge");
     let kişisel_bölgeler = document.getElementsByClassName("kişisel-bölge");
     
@@ -101,5 +105,112 @@ function KişiseldenAnonime()
         {
             anonim_bölgeler[i].style.display = "block";
         }
+    }
+}
+
+/**
+ * Yetki gerektiren sayfa ögelerini oluşturur.
+ */
+async function RoleGöreÖgeleriYarat()
+{
+    if (şimdi_kullanan === null)
+    {
+        return;
+    }
+
+    let ana_sayfa = document.getElementById("kişisel-çekmece-ana");
+    if (ana_sayfa === null)
+    {
+        return;
+    }
+
+    let oturum_kimliği = ÇerezDeğeri("OTURUM");
+
+    let yanıt = await fetch(
+        `${adres}Roller/NoktaEkleyebilirim`,
+        {
+            method: "POST",
+            headers: {
+                KULLANICI: şimdi_kullanan.Kimlik,
+                OTURUM: oturum_kimliği
+            }
+        }
+    );
+    // Nokta ekleme yetkisi varsa
+    if (yanıt.status == 200)
+    {
+        let NoktaEkleDüğmesi = document.createElement("button");
+        NoktaEkleDüğmesi.setAttribute("class", "D1-turkuaz butonlar-profil role-bağlı");
+        NoktaEkleDüğmesi.setAttribute("onclick", "KÇNoktaEkleSayfası(); " +
+            "KÇ_NoktaEkle_BölgeTürleriniGetir(); KÇ_NoktaEkle_ÜstBölgeleriGetir()");
+        NoktaEkleDüğmesi.innerHTML = "Nokta Ekle";
+        ana_sayfa.appendChild(NoktaEkleDüğmesi);
+    }
+
+    yanıt = await fetch(
+        `${adres}Roller/RolAtayabilirimAlabilirim`,
+        {
+            method: "POST",
+            headers: {
+                KULLANICI: şimdi_kullanan.Kimlik,
+                OTURUM: oturum_kimliği
+            }
+        }
+    );
+    // Rol atama ve alma yetkisi varsa
+    if (yanıt.status == 200)
+    {
+        let RolAtaDüğmesi = document.createElement("button");
+        RolAtaDüğmesi.setAttribute("class", "D1-turkuaz butonlar-profil role-bağlı");
+        RolAtaDüğmesi.setAttribute("onclick", "KÇRolAtaSayfası()");
+        RolAtaDüğmesi.innerHTML = "Rol Ata";
+        ana_sayfa.appendChild(RolAtaDüğmesi);
+
+        let RolAlDüğmesi = document.createElement("button");
+        RolAlDüğmesi.setAttribute("class", "D1-turkuaz butonlar-profil role-bağlı");
+        RolAlDüğmesi.setAttribute("onclick", "KÇRolAlSayfası()");
+        RolAlDüğmesi.innerHTML = "Rol Al";
+        ana_sayfa.appendChild(RolAlDüğmesi);
+    }
+
+    let NoktaPenceresiİçi = document.getElementById("nokta-penceresi-içorta");
+    if (NoktaPenceresiİçi === null)
+    {
+        return;
+    }
+
+    yanıt = await fetch(
+        `${adres}Roller/NoktaDüzenleyebilirim`,
+        {
+            method: "POST",
+            headers: {
+                KULLANICI: şimdi_kullanan.Kimlik,
+                OTURUM: oturum_kimliği
+            }
+        }
+    );
+    // Nokta düzenleme yetkisi varsa
+    if (yanıt.status == 200)
+    {
+        let NoktaDüzenleDüğmesi = document.createElement("button");
+        NoktaDüzenleDüğmesi.setAttribute("class", "favori-butonu role-bağlı");
+        NoktaDüzenleDüğmesi.setAttribute("onclick", "NoktaDuzenleSayfası(this)");
+        NoktaDüzenleDüğmesi.setAttribute("id", "nokta-duzenle-butonu");
+        NoktaDüzenleDüğmesi.setAttribute("konum-kimliği", "");
+        NoktaDüzenleDüğmesi.innerHTML = "Noktayı Düzenle";
+        NoktaPenceresiİçi.appendChild(NoktaDüzenleDüğmesi);
+    }
+}
+
+/**
+ * Yetki gerektiren sayfa ögelerini siler.
+ */
+async function RoleGöreÖgeleriYokEt()
+{
+    let role_göre = document.getElementsByClassName("role-bağlı");
+
+    for (öge of role_göre)
+    {
+        öge.remove();
     }
 }
